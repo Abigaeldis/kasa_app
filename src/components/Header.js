f6c2511b981e8a7d5c5b2cb6b882f0a731ea0cb0
whaleetv <65315128+whaleetv@users.noreply.github.com>
2023-05-09 18:06:57 +0200

    creation du header, img, link, logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,32 @@ import logo from "../assets/vectorhouse.png";
 import logoK from "../assets/vectorK.png";
 import logoS from "../assets/vectorS.png";
 import logoA from "../assets/vectorA.png";
+import paysage01 from "../assets/paysage01.png";
 import "../styles/Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <div className="logokasa">
-        {" "}
-        <img src={logoK} alt="LogoK" />
-        <img src={logo} alt="Logo" />
-        <img src={logoS} alt="LogoS" />
-        <img src={logoA} alt="LogoA" />
-      </div>
       <div className="navbar">
-        <a href="http://localhost:3000/" className="link-1">
-          Lien 1
-        </a>
-        <a href="http://localhost:3000/" className="link-2">
-          Lien 2
-        </a>
+        <div className="logokasa">
+          {" "}
+          <img src={logoK} alt="LogoK" />
+          <img src={logo} alt="Logo" />
+          <img src={logoS} alt="LogoS" />
+          <img src={logoA} alt="LogoA" />
+        </div>
+        <div className="navbar">
+          <a href="http://localhost:3000/" className="link-1">
+            Acceuil
+          </a>
+          <a href="http://localhost:3000/" className="link-2">
+            A propos
+          </a>
+        </div>
+      </div>
+      <div className="banner">
+        <img src={paysage01} alt="paysage côtier" />
+        <p>Chez vous, partout et ailleurs</p>
       </div>
     </div>
   );
