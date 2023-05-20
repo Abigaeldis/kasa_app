3b8ac358f37da0351200b4503bcbf00086085f9a
whaleetv <65315128+whaleetv@users.noreply.github.com>
2023-05-20 11:20:04 +0200

    add a button to expand more lodgings in gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,34 @@
 import "../styles/gallery.css";
 import lodgingList from "../datas/lodgingList.json";
+import React, { useState } from "react";
 import Card from "./Card";
 
 function Gallery() {
+  const initialVisibleCount = 6; // Initial number of visible cards
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(initialVisibleCount);
+  const handleShowMore = () => {
+    setIsExpanded(true);
+    setVisibleCount(lodgingList.length); // Show all cards
+  };
+  const handleHideContent = () => {
+    setIsExpanded(false);
+    setVisibleCount(initialVisibleCount); // Show initial number of cards
+  };
+
+  const visibleLodgingList = lodgingList.slice(0, visibleCount);
+
   return (
-    <div className="gallery">
-      {lodgingList.map((lodging) => (
-        <Card
-          key={lodging.id}
-          title={lodging.title}
-          image={lodging.pictures[0]}
-          //   description={lodging.description}
-          link={`/${lodging.id}`}
-        />
-      ))}
+    <div>
+      <div className="gallery">
+        {visibleLodgingList.map((lodging) => (
+          <Card key={lodging.id} title={lodging.title} image={lodging.pictures[0]} link={lodging.link} />
+        ))}
+      </div>
+      <div className="button-container">
+        {!isExpanded && visibleCount < lodgingList.length && <button onClick={handleShowMore}>Show More</button>}
+        {isExpanded && <button onClick={handleHideContent}>Hide Content</button>}
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import "../styles/home.css";
 import Header from "../components/Header";
 import Gallery from "../components/Gallery";
 import paysage01 from "../assets/paysage01.png";
+// import lodgingList from "../datas/lodgingList.json";
 
 function Home() {
   return (
