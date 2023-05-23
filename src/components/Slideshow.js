9ca42f7feb74f7e791f52d184a32fb83a154b86e
whaleetv <65315128+whaleetv@users.noreply.github.com>
2023-05-23 09:50:31 +0200

    sizing the slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../styles/slideshow.css";
 
 function Slideshow({ images }) {
   return (
     <Carousel showArrows={true} showStatus={false} showIndicators={false} showThumbs={false}>
       {images.map((image, index) => (
         <div key={index}>
-          <img src={image} alt={`Numero ${index}`} />
+          <div className="image-wrapper">
+            <img src={image} alt={`Numero ${index}`} />
+          </div>
         </div>
       ))}
     </Carousel>
