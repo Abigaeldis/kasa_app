874f5abdec728015f311c3446d5ef6cf3defb081
whaleetv <65315128+whaleetv@users.noreply.github.com>
2023-05-23 10:11:14 +0200

    changed the way slideshow was made

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "../styles/slideshow.css";
+import React, { useState } from "react";
 
 function Slideshow({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
-    <Carousel showArrows={true} showStatus={false} showIndicators={false} showThumbs={false}>
-      {images.map((image, index) => (
-        <div key={index}>
-          <div className="image-wrapper">
-            <img src={image} alt={`Numero ${index}`} />
-          </div>
-        </div>
-      ))}
-    </Carousel>
+    <div>
+      <button onClick={goToPrevious}>Previous</button>
+      <img src={images[currentIndex]} alt={`Numero ${currentIndex}`} />
+      <button onClick={goToNext}>Next</button>
+    </div>
   );
 }
 
