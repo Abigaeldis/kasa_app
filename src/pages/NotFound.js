82034747f2d109e16952234263cb190c6b39fd0d
whaleetv <65315128+whaleetv@users.noreply.github.com>
2023-05-26 15:25:06 +0200

    styling the notfound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import "../styles/notfound.css";
+import { NavLink } from "react-router-dom";
 
 function NotFound() {
   return (
     <div>
       <Header />
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <div className="notfound">
+        <h1>404</h1>
+        <p className="errormessage">Oups! La page que vous demandez n'existe pas.</p>
+        <NavLink to="/" className="nav-active homeLink">
+          <p>Retourner sur la page d’accueil</p>
+        </NavLink>
+      </div>
     </div>
   );
 }
